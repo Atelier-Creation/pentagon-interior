dfb2e346043a3587ae96058db6a9d60cb355d35c
prasanthoyasco <prasanthoyasco@gmai.com>
2026-04-17 19:06:43 +0530

    home page made mobile responcive

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import HomePage from './homePage'
 import Contact from './Contact'
 import About from "./About";
 import Services from "./Services";
-<<<<<<< HEAD
 import Projects from "./Projects";
 import ProjectDetail from "./ProjectDetail";
 import ScrollToTop from "./ScrollToTop";
-=======
 import ExecutionDelivery from "./ExecutionDelivery";
->>>>>>> 83ef488e57782b2db1b9bd26741ab9928fee945b
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -22,12 +19,9 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-<<<<<<< HEAD
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
-=======
         <Route path="/execution-delivery" element={<ExecutionDelivery />} />
->>>>>>> 83ef488e57782b2db1b9bd26741ab9928fee945b
       </Routes>
     </Router>
   )
